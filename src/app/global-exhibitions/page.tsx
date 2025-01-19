17ff4086201860ebf6c1854b7a13c31a60fb84ab
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2025-01-19 04:43:46 +0100

    loading + error on global exhition

--- src/app/global-exhibitions/page.tsx
+++ src/app/global-exhibitions/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Header, Timeline } from "components/custom";
+import { Skeleton } from "components/components/ui/skeleton";
+import { Header, Timeline, TimelineSkeleton } from "components/custom";
 import { useData } from "hooks/useData";
 import { useState } from "react";
 import dynamic from "next/dynamic";
@@ -18,25 +19,54 @@ export default function GlobalExhibitions() {
     setSelectedYear(year);
   };
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error || !data) return <div>Error: {error?.message}</div>;
-
-  return (
-
+  if (isLoading) {
+    return (
       <div className="h-screen bg-latte text-espresso font-serif flex flex-col">
         <Header />
         <main className="flex-1 container mx-auto px-4 py-4 flex flex-col">
           <div className="flex gap-4 w-full justify-between items-center mb-4">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-left lg:text-center text-coffee">
-              Global Exhibitions Map
-            </h1>
-            <Timeline onYearChange={handleYearChange} />
+            <Skeleton>
+              <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-left lg:text-center text-coffee opacity-0">
+                Global Exhibitions Map
+              </h1>
+            </Skeleton>
+            <TimelineSkeleton />
           </div>
           <div className="p-2 flex-1 bg-cream rounded-lg shadow-md">
-            <ExhibitionMap data={data} selectedYear={selectedYear} />
+            <Skeleton className="h-full w-full" />
           </div>
         </main>
       </div>
+    );
+  }
 
+  if (error || !data) {
+    return (
+      <div className="h-screen bg-latte text-espresso font-serif flex flex-col items-center justify-center text-center bg-cream p-6 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-espresso mb-4">
+          Oops! Something went wrong.
+        </h2>
+        <p className="text-espresso text-lg">
+          {error?.message || "Error no data"}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-screen bg-latte text-espresso font-serif flex flex-col">
+      <Header />
+      <main className="flex-1 container mx-auto px-4 py-4 flex flex-col">
+        <div className="flex gap-4 w-full justify-between items-center mb-4">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-left lg:text-center text-coffee">
+            Global Exhibitions Map
+          </h1>
+          <Timeline onYearChange={handleYearChange} />
+        </div>
+        <div className="p-2 flex-1 bg-cream rounded-lg shadow-md">
+          <ExhibitionMap data={data} selectedYear={selectedYear} />
+        </div>
+      </main>
+    </div>
   );
 }
